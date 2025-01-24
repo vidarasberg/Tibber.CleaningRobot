7e72951c03c30fe1822697c079fcdf3cb1e1d27f
Vidar Åsberg <vidar.asberg@pm.me>
2025-01-24 16:44:32 +0100

    adds enum serialization and applies validation filter

--- Tibber.CleaningBotWebAPI/Program.cs
+++ Tibber.CleaningBotWebAPI/Program.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+using Tibber.CleaningBotWebAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://+:5000");
 
@@ -5,21 +10,44 @@ builder.WebHost.UseUrls("http://+:5000");
 builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 var app = builder.Build();
 app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.MapPost("/tibber-developer-test/enter-path", () =>
+RouteGroupBuilder group = app.MapGroup("/tibber-developer-test");
+group.MapPost("/enter-path", (RobotRequest body) =>
     {
-        throw new NotImplementedException();
+        Stopwatch stopWatch = new();
+
+        stopWatch.Start();
+        RobotCalculator.CalculateUniquePlacesCleaned(body.Start, body.Commands);
+        stopWatch.Stop();
+        return Results.Ok(stopWatch.Elapsed.Seconds);
     })
-    .WithName("EnterPath");
+    .WithName("EnterPath")
+    .AddEndpointFilter<RobotRequestValidationFilter>()
+    .ProducesProblem(StatusCodes.Status400BadRequest);
 
 app.Run();
+
+
+public enum Direction
+{
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
+}
